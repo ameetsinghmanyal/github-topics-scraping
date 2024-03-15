faaa4456ee123286d97a934acebd316931fd9aa5
Ameet Singh Manyal <ameetsinghmanyal@gmail.com>
2024-03-15 14:38:47 +0530

    Added necessary comments for the get_topics_page function.

--- get_topics.py
+++ get_topics.py
@@ -1,1 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
 
+def get_topics_page():
+    # Get status of the requested page from github.com
+    topics_url = 'https://github.com/topics'
+    response = requests.get(topics_url)
+    if response.status_code != 200:
+        raise Exception('Failed to load page {}'.format(topic_url))
+    doc = BeautifulSoup(response.text, 'html.parser')
+    return doc
