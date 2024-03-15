2e5966a1734521408691be6091845e05981e61d4
Ameet Singh Manyal <ameetsinghmanyal@gmail.com>
2024-03-15 14:55:19 +0530

    Added function get_topic_titles

--- get_topics.py
+++ get_topics.py
@@ -9,3 +9,12 @@ def get_topics_page():
         raise Exception('Failed to load page {}'.format(topic_url))
     doc = BeautifulSoup(response.text, 'html.parser')
     return doc
+
+def get_topic_titles(doc):
+    # Get topic titles
+    selection_class = 'f3 lh-condensed mb-0 mt-1 Link--primary'
+    topic_title_tags = doc.find_all('p', {'class': selection_class})
+    topic_titles = []
+    for tag in topic_title_tags:
+        topic_titles.append(tag.text)
+    return topic_titles
