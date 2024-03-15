717bb420b29d658c7e6475de2677fb80c37c4caf
Ameet Singh Manyal <ameetsinghmanyal@gmail.com>
2024-03-15 17:18:12 +0530

    Added final functions to save data in csv

--- get_topics.py
+++ get_topics.py
@@ -1,6 +1,17 @@
+"""
+    The code scrapes the list of topics from GitHub, retrieves the top repositories for each topic, and
+    saves the data into CSV files.
+    :return: The code is returning a list of topics from GitHub along with their descriptions and URLs.
+    It then scrapes the top repositories for each topic and saves the information in CSV files in a
+    'data' directory.
+"""
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import os
+
+base_url = 'https://github.com'
 
 def get_topics_page():
     # Get status of the requested page from github.com
@@ -50,3 +61,66 @@ def scrape_topics():
     }
     #print(pd.DataFrame(topics_dict))
     return pd.DataFrame(topics_dict)
+
+def get_topic_page(topic_url):
+    # Download the page
+    response = requests.get(topic_url)
+    # Check successful response
+    if response.status_code != 200:
+        raise Exception('Failed to load page {}'.format(topic_url))
+    # Parse using Beautiful soup
+    topic_doc = BeautifulSoup(response.text, 'html.parser')
+    return topic_doc
+
+def parse_star_count(stars):
+    stars=stars.strip()
+    if stars[-1]=='k':
+        return int(float(stars[:-1])*1000)
+    return(int(stars))
+
+def get_repo_info(h1_tag, star_tag):
+    # returns all the required info about a repository
+    a_tags = h1_tag.find_all('a')
+    username = a_tags[0].text.strip()
+    repo_name = a_tags[1].text.strip()
+    repo_url =  base_url + a_tags[1]['href']
+    stars = parse_star_count(star_tag.text.strip())
+    return username, repo_name, stars, repo_url
+
+def get_topic_repos(topic_doc):
+    # Get the h1 tags containing repo title, repo URL and username
+    repo_tags = topic_doc.find_all('article',{'class':'border rounded color-shadow-small color-bg-subtle my-4'})
+
+    # Get star tags
+    star_tags=topic_doc.find_all('span',{'id':'repo-stars-counter-star'})
+    
+    topic_repos_dict = { 'username': [], 'repo_name': [], 'stars': [],'repo_url': []}
+
+    # Get repo info
+    for i in range(len(repo_tags)):
+        repo_info = get_repo_info(repo_tags[i], star_tags[i])
+        topic_repos_dict['username'].append(repo_info[0])
+        topic_repos_dict['repo_name'].append(repo_info[1])
+        topic_repos_dict['stars'].append(repo_info[2])
+        topic_repos_dict['repo_url'].append(repo_info[3])
+        
+    return pd.DataFrame(topic_repos_dict)
+
+def scrape_topic(topic_url, path):
+    if os.path.exists(path):
+        print("The file {} already exists. Skipping...".format(path))
+        return
+    topic_df = get_topic_repos(get_topic_page(topic_url))
+    topic_df.to_csv(path, index=None)
+
+# Putting it all together below
+def scrape_topics_repos():
+    print('Scraping list of topics')
+    topics_df = scrape_topics()
+    
+    os.makedirs('data', exist_ok=True)
+    for index, row in topics_df.iterrows():
+        print('Scraping top repositories for "{}"'.format(row['title']))
+        scrape_topic(row['url'], 'data/{}.csv'.format(row['title']))
+
+scrape_topics_repos()
